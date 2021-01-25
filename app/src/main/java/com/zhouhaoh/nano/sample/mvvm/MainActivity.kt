7affc7e2644f7a995e1b10d55f577a10b9c50c7b
zhouhaoo <zhouhao7@icloud.com>
2021-01-25 15:07:36 +0800

    解除view层耦合

--- app/src/main/java/com/zhouhaoh/nano/sample/mvvm/MainActivity.kt
+++ app/src/main/java/com/zhouhaoh/nano/sample/mvvm/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.zhouhaoh.nano.sample.mvvm
 
-import com.zhouhaoh.nano.ui.base.BaseActivity
-import com.zhouhaoh.nano.sample.R
 import com.zhouhaoh.nano.sample.base.applyState
-import com.zhouhaoh.nano.sample.databinding.ActivityMainBinding
-import com.zhouhaoh.nano.ui.ext.bindingView
+import com.zhouhaoh.nano.ui.base.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity() {
     val viewModel by viewModel<SampleViewModel>()
-    private val binding by bindingView<MainActivity, ActivityMainBinding>()
-    override val layoutResID: Int
-        get() = R.layout.activity_main
-
     override fun initData() {
         applyState(viewModel)
-        binding.viewModel = viewModel
+
     }
 }
